dfc7d8e779cc8167d7ff1c5f5df41d8ec331ccb7
Mohamed-Bekhouche <bekhouch@sheridancollege.ca>
2023-05-25 14:31:20 -0400

    InClass version 1.0

---
+++ src/software_d_fund/Software_D_fund.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package software_d_fund;
+
+/**
+ *
+ * @author medbe
+ */
+import java.util.Scanner;
+public class Software_D_fund {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Student[] list = new Student[3] ;
+        Scanner in = new Scanner(System.in) ;
+        for (int i = 0 ; i <list.length;i++)
+        {
+            System.out.print("Enter name of the " + (i+1) + " student:  ");
+            String n = in.next() ;
+            System.out.print("Enter age of the " + (i+1) + " student:  ");
+            int a = in.nextInt();
+            Student s1 = new Student(n,a) ;
+            list[i] = s1 ; 
+        }
+        for(int i = 0 ; i <list.length ; i++)
+        {
+            System.out.println(list[i].getName());
+            System.out.println(list[i].getAge());
+        }
+    }
+    
+}
---
+++ src/software_d_fund/Student.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package software_d_fund;
+
+/**
+ *
+ * @author medbe
+ */
+public class Student 
+{
+    private String name ;
+    private int age ;
+    public Student(String n, int a)
+    {
+        this.name=n ; 
+        this.age =a;
+    }
+    public String getName ()
+    {
+        return name;
+    }
+    public  int getAge ()
+    {
+        return age ;
+    }
+    
+}
